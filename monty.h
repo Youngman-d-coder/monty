cbb8e13b8764874726390b166fdc5bc46fec81df
youngman-d-coder <sayhitonelson@gmail.com>
2024-02-01 05:22:13 +0100

    update monty.h

--- monty.h
+++ monty.h
@@ -7,5 +7,20 @@
 #include <stlib.h>
 #include <string.h>
 
+/* STRUCTURES */
+typedef struct stack_s
+{
+  int n;
+  struct stack_s *prev;
+  struct stack_s *next;
+} stack_t;
+
+typedef struct instruction_s
+{
+  char *opcode;
+  void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/* PROTOTYPES */
 
 #endif /* MONTY_H */
